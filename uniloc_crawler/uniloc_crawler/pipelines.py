51a42ca3fabdfd13ef0d10c889fc2c6ec1496175
Diogo Rafael Silva Castro <diogocastro56@gmail.com>
2021-11-03 17:14:02 +0000

    UL5

--- uniloc_crawler/uniloc_crawler/pipelines.py
+++ uniloc_crawler/uniloc_crawler/pipelines.py
@@ -9,21 +9,22 @@ from itemadapter import ItemAdapter
 import mysql.connector
 import logging
 
+
 class InstituicoesPipeline:
 
     def open_spider(self, spider):
         self.create_connection()
         logging.info("\n\n SPIDER INSTITUICOES \n\n")
-    
+
     def create_connection(self):
         self.conn = mysql.connector.connect(
-            host = 'localhost',
-            user = 'root',
-            passwd = '',
-            database = 'projeto_final'
+            host='localhost',
+            user='root',
+            passwd='',
+            database='projeto_final'
         )
         self.curr = self.conn.cursor()
-    
+
     def process_item(self, item, spider):
         self.store_db(item)
         return item
@@ -37,31 +38,31 @@ class InstituicoesPipeline:
         ))
 
         self.conn.commit()
-    
+
     def close_spider(self, spider):
         logging.info("\n\n FECHANDO SPIDER INSTITUICOES \n\n")
         self.client.close()
+
 
 class DistritosPipeline:
 
     def open_spider(self, spider):
         self.create_connection()
         logging.info("\n\n SPIDER DISTRITOS \n\n")
-    
+
     def create_connection(self):
         self.conn = mysql.connector.connect(
-            host = 'localhost',
-            user = 'root',
-            passwd = '',
-            database = 'projeto_final'
+            host='localhost',
+            user='root',
+            passwd='',
+            database='projeto_final'
         )
         self.curr = self.conn.cursor()
-    
+
     def process_item(self, item, spider):
         if item['id'] and item['nome']:
             self.store_db(item)
             return item
-            
 
     def store_db(self, item):
         self.curr.execute(""" INSERT INTO `distritos`(`ID`, `nome`) VALUES (%s,%s)""", (
@@ -70,87 +71,113 @@ class DistritosPipeline:
         ))
 
         self.conn.commit()
-    
+
     def close_spider(self, spider):
         logging.info("\n\n FECHANDO SPIDER DISTRITOS \n\n")
         self.client.close()
 
+
 class CidadesPipeline:
-    
+
     def open_spider(self, spider):
         self.create_connection()
         logging.info("\n\n SPIDER CIDADES \n\n")
-    
+
     def create_connection(self):
         self.conn = mysql.connector.connect(
-            host = 'localhost',
-            user = 'root',
-            passwd = '',
-            database = 'projeto_final'
+            host='localhost',
+            user='root',
+            passwd='',
+            database='projeto_final'
         )
         self.curr = self.conn.cursor()
-    
 
     # Apesar de enviarmos muitos registos com a PK duplicada devido á formatação do codigo postal, a BD só irá aceitar aqueles que não são duplicados, exatamente por serem PK
     # Efetuar uma query a todos os items seria mais demoro que deixar a BD lidar com a situação
+
     def process_item(self, item, spider):
         if item['nome'] and item['dist_id']:
             self.store_db(item)
             return item
-            
+
     def store_db(self, item):
-        
+
         self.curr.execute(""" INSERT INTO `cidades`(`nome`, `distritos_ID`, `qualidade`) VALUES (%s,%s,'N/A')""", (
             item['nome'],
             item['dist_id'],
         ))
 
         self.conn.commit()
-    
+
     def close_spider(self, spider):
         logging.info("\n\n FECHANDO SPIDER CIDADES \n\n")
         self.client.close()
+
 
 class CodPostaisPipeline:
     def open_spider(self, spider):
         self.create_connection()
         logging.info("\n\n SPIDER CODIGOS POSTAIS \n\n")
-    
+
     def create_connection(self):
         self.conn = mysql.connector.connect(
-            host = 'localhost',
-            user = 'root',
-            passwd = '',
-            database = 'projeto_final'
+            host='localhost',
+            user='root',
+            passwd='',
+            database='projeto_final'
         )
         self.curr = self.conn.cursor()
-    
 
     # Apesar de enviarmos muitos registos com a PK duplicada devido á formatação do codigo postal, a BD só irá aceitar aqueles que não são duplicados, exatamente por serem PK
     # Efetuar uma query a todos os items seria mais demoro que deixar a BD lidar com a situação
+
     def process_item(self, item, spider):
 
         if item['cod'] and item['nome']:
             self.store_db(item)
             return item
-            
 
     def store_db(self, item):
-        
+
         self.curr.execute(""" INSERT INTO `codigos_postais`(`cod_postal`, `cidades_nome`) VALUES (%s,%s) """, (
             item['cod'][:4],
             item['nome'],
         ))
 
         self.conn.commit()
-    
+
     def close_spider(self, spider):
         logging.info("\n\n FECHANDO SPIDER CIDADES \n\n")
         self.client.close()
 
 
+class RankPipeline:
+    def open_spider(self, spider):
+        self.create_connection()
+        logging.info("\n\n SPIDER Ranking \n\n")
 
+    def create_connection(self):
+        self.conn = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            passwd='',
+            database='projeto_final'
+        )
+        self.curr = self.conn.cursor()
 
+    def process_item(self, item, spider):
+        if item['rank'] is not None and item['nome'] is not None:
+            self.store_db(item)
+            return item
 
+    def store_db(self, item):
+        self.curr.execute(""" UPDATE `instituicoes` SET `Rank` = %s WHERE `nome` LIKE %s """, (
+            item['rank'],
+            item['nome'] + "%",
+        ))
 
+        self.conn.commit()
 
+    def close_spider(self, spider):
+        logging.info("\n\n FECHANDO SPIDER RANKING \n\n")
+        self.client.close()
--- uniloc_crawler/uniloc_crawler/spiders/dges_spider.py
+++ uniloc_crawler/uniloc_crawler/spiders/dges_spider.py
@@ -2,7 +2,7 @@ import scrapy
 from scrapy.crawler import CrawlerProcess
 
 
-#Lista de instituições e os seus respetivos cursos
+# Lista de instituições e os seus respetivos cursos
 class dgesSpider(scrapy.Spider):
     #
     #   ESTE SCRIPT NÃO CORRESPONDE A NENHUMA TABELA
@@ -19,38 +19,42 @@ class dgesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        #.box9 é a classe que engloba os titulos de instituições
+        # .box9 é a classe que engloba os titulos de instituições
         for post in response.css('.box9'):
 
             # Por cada instituição, criamos um dicionario para todos os seus cursos
             cursos = list()
-            
-            concat = " + .lin-ce " # seletor css para o proximo irmão (sibling)
 
-            #Sintetizamos um bloco do while
+            # seletor css para o proximo irmão (sibling)
+            concat = " + .lin-ce "
+
+            # Sintetizamos um bloco do while
             # curso['nome'] = post.css('.box9 '+concat+' a::text' ).get()
             # cursos.append(curso)
             # concat = concat + " + .lin-ce "
 
-            while(post.css('.box9 '+ concat  ).get() is not None):
+            while(post.css('.box9 ' + concat).get() is not None):
                 curso = dict()
-                curso['cod'] = post.css('.box9 '+concat+' .lin-ce-c2::text' ).get()
-                curso['nome'] = post.css('.box9 '+concat+' .lin-ce-c3 a::text' ).get()
+                curso['cod'] = post.css(
+                    '.box9 '+concat+' .lin-ce-c2::text').get()
+                curso['nome'] = post.css(
+                    '.box9 '+concat+' .lin-ce-c3 a::text').get()
 
                 cursos.append(curso)
                 concat = concat + " + .lin-ce "
-                
 
             yield {
                 'cod': post.css('div::text')[0].get(),
                 'nome': post.css('div::text')[1].get(),
-                'cod_tipo' : response.url[-2:],
-                'cursos' : cursos
+                'cod_tipo': response.url[-2:],
+                'cursos': cursos
             }
 
 # Popular a tabela instituições
+
+
 class instCrawler(scrapy.Spider):
-    
+
     name = "insts"
 
     custom_settings = {
@@ -75,30 +79,29 @@ class instCrawler(scrapy.Spider):
     # 22 - Ensino Superior Privado Universitário
 
     def parse(self, response):
-        #.box9 é a classe que engloba os titulos de instituições
+        # .box9 é a classe que engloba os titulos de instituições
         for div in response.css('.box9'):
-                cod = div.css('div::text')[0].get()
-                nome = div.css('div::text')[1].get()
-                cod_tipo = response.url[-2:]
-                link = response.urljoin(div.css('.box9 + div .lin-ce-c3 a::attr(href)').get())
-                yield scrapy.Request(url = link, callback=self.parse_codigo, meta={'cod' : cod, 'nome' : nome, 'cod_tipo' : cod_tipo})
-               
-            
-    
+            cod = div.css('div::text')[0].get()
+            nome = div.css('div::text')[1].get()
+            cod_tipo = response.url[-2:]
+            link = response.urljoin(
+                div.css('.box9 + div .lin-ce-c3 a::attr(href)').get())
+            yield scrapy.Request(url=link, callback=self.parse_codigo, meta={'cod': cod, 'nome': nome, 'cod_tipo': cod_tipo})
+
     async def parse_codigo(self, response):
         cod = response.request.meta['cod']
         nome = response.request.meta['nome']
         cod_tipo = response.request.meta['cod_tipo']
         cod_postal = await self.get_cod(response.xpath("/html/body/div[2]/div/div/div/div/div/div[2]/div/div[5]/text()").extract())
         yield {
-                    'cod': cod,
-                    'nome': nome,
-                    'cod_tipo' : cod_tipo,
-                    'cod_postal' : cod_postal
-                }
-    
+            'cod': cod,
+            'nome': nome,
+            'cod_tipo': cod_tipo,
+            'cod_postal': cod_postal
+        }
+
     async def get_cod(self, lista):
-        
+
         cod_postal = 0000
         for item in lista:
             candt = item[:4]
@@ -107,9 +110,6 @@ class instCrawler(scrapy.Spider):
                 break
         return cod_postal
 
-        
-
-       
 
 # Popular a tabela cursos
 class cursoCrawler(scrapy.Spider):
@@ -123,19 +123,22 @@ class cursoCrawler(scrapy.Spider):
         return chr((ord(s.upper())+1 - 65) % 26 + 65)
 
     def parse(self, response):
-        #.box9 é a classe que engloba os titulos de instituições
+        # .box9 é a classe que engloba os titulos de instituições
         for post in response.css('.box10'):
             yield {
                 'cod': post.css('div::text')[0].get(),
                 'nome': post.css('div::text')[1].get(),
             }
-        
-        next = response.request.url[-1] # Mudamos um A para o B
-        link = "https://dges.gov.pt/guias/indcurso.asp?letra=" + self.next_alpha(next)
+
+        next = response.request.url[-1]  # Mudamos um A para o B
+        link = "https://dges.gov.pt/guias/indcurso.asp?letra=" + \
+            self.next_alpha(next)
         if next:
             yield scrapy.Request(url=link, callback=self.parse)
 
 # Popular a tabela associativa inst_curso
+
+
 class inst_cursoCrawler(scrapy.Spider):
     name = "inst_curso"
 
@@ -149,21 +152,22 @@ class inst_cursoCrawler(scrapy.Spider):
     ]
 
     def parse(self, response):
-        #.box9 é a classe que engloba os titulos de instituições
+        # .box9 é a classe que engloba os titulos de instituições
         for post in response.css('.box9'):
 
             cod_inst = post.css('div::text')[0].get()
-            
-            concat = " + .lin-ce " # seletor css para o proximo irmão (sibling)
-            while(post.css('.box9 '+ concat  ).get() is not None):
-                cod_curso = post.css('.box9 '+concat+' .lin-ce-c2::text' ).get()
 
-                link = response.urljoin(post.css('.box9 '+concat+' .lin-ce-c3 a::attr(href)' ).get())
-                yield scrapy.Request(url = link, callback=self.parse_ult, meta={'curso' : cod_curso, 'inst' : cod_inst})
-                
+            # seletor css para o proximo irmão (sibling)
+            concat = " + .lin-ce "
+            while(post.css('.box9 ' + concat).get() is not None):
+                cod_curso = post.css('.box9 '+concat+' .lin-ce-c2::text').get()
+
+                link = response.urljoin(
+                    post.css('.box9 '+concat+' .lin-ce-c3 a::attr(href)').get())
+                yield scrapy.Request(url=link, callback=self.parse_ult, meta={'curso': cod_curso, 'inst': cod_inst})
+
                 concat = concat + " + .lin-ce "
 
-    
     def parse_ult(self, response):
         curso = response.request.meta['curso']
         inst = response.request.meta['inst']
@@ -183,17 +187,19 @@ class inst_cursoCrawler(scrapy.Spider):
                 'curso': curso,
                 'inst': inst,
                 'nota_ult_EN': nota_ult_EN,
-                'nota_ult_ER' : nota_ult_ER
+                'nota_ult_ER': nota_ult_ER
             }
         except:
-             yield{
+            yield{
                 'curso': curso,
                 'inst': inst,
                 'nota_ult_EN': "Informação não disponibilizada",
-                'nota_ult_ER' : "Informação não disponibilizada"
+                'nota_ult_ER': "Informação não disponibilizada"
             }
 
 # Popular a tabela distritos
+
+
 class distCrawler(scrapy.Spider):
     name = "dist"
 
@@ -210,10 +216,11 @@ class distCrawler(scrapy.Spider):
     def parse(self, response):
 
         for row in response.css('.table_milieu tr'):
-                yield {
-                    'nome' : row.css("a::text").get(),
-                    'id' : row.css("td.gras::text").get()
-                }
+            yield {
+                'nome': row.css("a::text").get(),
+                'id': row.css("td.gras::text").get()
+            }
+
 
 class cod_postCrawler(scrapy.Spider):
     name = "cod_post"
@@ -233,31 +240,33 @@ class cod_postCrawler(scrapy.Spider):
         for row in response.css('.table_milieu tr'):
             if row.css("a::attr(href)").get():
                 link = response.urljoin(row.css("a::attr(href)").get())
-                yield scrapy.Request(url = link, callback=self.parse_cidade)
-    
+                yield scrapy.Request(url=link, callback=self.parse_cidade)
+
     def parse_cidade(self, response):
 
-        dist_cod = response.css("h1::text").get()[-3:-1] # O penultimo e ante-penultimo caracter
+        # O penultimo e ante-penultimo caracter
+        dist_cod = response.css("h1::text").get()[-3:-1]
         for row in response.css('.table_milieu tr '):
 
             cid_nome = row.css("a::text").get()
             link = response.urljoin(row.css("a::attr(href)").get())
 
-            yield scrapy.Request(url = link, callback=self.parse_codigos, meta = {'dist_cod' : dist_cod, 'cid_nome' : cid_nome})
-    
+            yield scrapy.Request(url=link, callback=self.parse_codigos, meta={'dist_cod': dist_cod, 'cid_nome': cid_nome})
+
     def parse_codigos(self, response):
         cid_nome = response.request.meta['cid_nome']
         dist_cod = response.request.meta['dist_cod']
-        
+
         for row in response.css(".table_milieu tr"):
             cod = row.css(" td:nth-child(2n):not(.xx)::text").get()
             yield {
-                'nome' : cid_nome,
-                'cod' : cod
+                'nome': cid_nome,
+                'cod': cod
             }
 
-            ##response.css(".table_milieu tr td:nth-child(2n):not(.xx)::text").getall() gets us the code
-        
+            # response.css(".table_milieu tr td:nth-child(2n):not(.xx)::text").getall() gets us the code
+
+
 class ciddCrawler(scrapy.Spider):
     name = "cidd"
 
@@ -276,20 +285,20 @@ class ciddCrawler(scrapy.Spider):
         for row in response.css('.table_milieu tr'):
             if row.css("a::attr(href)").get():
                 link = response.urljoin(row.css("a::attr(href)").get())
-                yield scrapy.Request(url = link, callback=self.parse_cidade)
-    
+                yield scrapy.Request(url=link, callback=self.parse_cidade)
+
     def parse_cidade(self, response):
 
-        dist_cod = response.css("h1::text").get()[-3:-1] # O penultimo e ante-penultimo caracter
+        # O penultimo e ante-penultimo caracter
+        dist_cod = response.css("h1::text").get()[-3:-1]
         for row in response.css('.table_milieu tr '):
 
             cid_nome = row.css("a::text").get()
 
             yield{
-                'nome' : cid_nome,
-                'dist_id' : dist_cod
+                'nome': cid_nome,
+                'dist_id': dist_cod
             }
-
 
 
 # process = CrawlerProcess()
@@ -306,10 +315,22 @@ class ciddCrawler(scrapy.Spider):
 # process.start() # the script will block here until all crawling jobs are finished
 
 
-        
-            
+class rankingCrawler(scrapy.Spider):
+    name = "rank"
 
-       
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'uniloc_crawler.pipelines.RankPipeline': 400
+        }
+    }
 
-            
-            
+    start_urls = [
+        'https://www.4icu.org/pt/'
+    ]
+
+    def parse(self, response):
+        for row in response.css('.table-hover tr'):
+            yield {
+                'rank': row.css("b::text").get(),
+                'nome': row.css("a::text").get()
+            }
