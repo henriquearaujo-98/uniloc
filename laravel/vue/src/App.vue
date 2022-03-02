30e8c72f69d447f13c96808ecf07b9711296bf3b
Henrique Araujo <ha_98@msn.com>
2022-03-02 11:34:24 -0100

    UL31 de novo. Apenas criar o projeto

--- laravel/vue/src/App.vue
+++ laravel/vue/src/App.vue
@@ -1,19 +1,30 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-
-</script>
-
 <template>
-    <router-view></router-view>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- laravel/vue/src/router/index.js
+++ laravel/vue/src/router/index.js
@@ -1,53 +1,25 @@
-import {createRouter, createWebHistory} from "vue-router";
-import Dashboard from "../views/Dashboard.vue";
-import Login from "../views/Login.vue";
-import Register from "../views/Register.vue";
-import DefaultLayout from "../components/DefaultLayout.vue";
-import Surveys from "../views/Surveys.vue";
-import store from "../store";
-
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const routes = [
-
-    {
-        path: '/',
-        redirect: '/dashboard',
-        name: 'Dashboard',
-        component: DefaultLayout,
-        meta: {requiresAuth : true},
-        children: [
-            {path: '/dashboard', name: 'Dashboard', component: Dashboard},
-            {path: '/surveys', name: 'Surveys', component: Surveys}
-        ]
-    },
-
-    {
-        path: '/login',
-        name: 'Login',
-        component: Login
-    },
-
-    {
-        path: '/register',
-        name: 'Register',
-        component: Register
-    }
-
-];
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHashHistory(),
+  routes
 })
 
-router.beforeEach((to, from, next) => {
-    if(to.meta.requiresAuth && !store.state.user.token){
-        next({name: 'Login'});
-    }else if (store.state.user.token && (to.name === 'Login' || to.name === 'Register')){
-        next({name: 'Dashboard'});
-    }else{
-        next();
-    }
-})
-
-export default router;
+export default router
