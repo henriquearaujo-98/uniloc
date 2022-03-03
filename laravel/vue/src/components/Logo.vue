bada37c8f123ec104b2f6a8ec74771d56bf1fe12
Henrique Araujo <ha_98@msn.com>
2022-03-03 22:24:02 -0100

    UL32 - Ficheiros iniciais

---
+++ laravel/vue/src/components/Logo.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <img src="../assets/logo-full.png">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Nav"
+}
+</script>
+
+<style scoped>
+img{
+    width: 250px;
+    height: auto;
+}
+</style>
