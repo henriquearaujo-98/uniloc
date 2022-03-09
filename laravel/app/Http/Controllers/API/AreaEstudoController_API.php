f17175eb3e58da2bfc127035c56ca4052657be26
Henrique Araujo <ha_98@msn.com>
2022-03-09 18:03:34 +0100

    UL35 - Refractor dos controllers do API

---
+++ laravel/app/Http/Controllers/API/AreaEstudoController_API.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Area_Estudo;
+use Illuminate\Http\Request;
+
+class AreaEstudoController_API extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Area_Estudo::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'ID' => 'required',
+            'nome' => 'required',
+        ]);
+
+        return Area_Estudo::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Area_Estudo::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $area_estudo = Area_Estudo::findOrFail($id);
+        return $area_estudo->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $area_estudo = Area_Estudo::findOrFail($id);
+        $area_estudo->delete();
+    }
+}
---
+++ laravel/app/Http/Controllers/API/ProvaIngressoController_API.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Prova_Ingresso;
+use Illuminate\Http\Request;
+
+class ProvaIngressoController_API extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Prova_Ingresso::all();
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function exames($curso_id, $instituicao_id)
+    {
+        $res = Prova_Ingresso::all()->where('cursoID', $curso_id)->where('instituicoes_ID', $instituicao_id)->first();
+        $ids = explode(',', $res->exames_id);
+        $nomes = array();
+        foreach ($ids as $id){
+            array_push($nomes,Exame::all()->where('Codigo',$id));
+        }
+
+        return $nomes;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'cursoID' => 'required',
+            'instituicoes_ID' => 'required',
+            'exames_ID' => 'required',
+        ]);
+
+        return Prova_Ingresso::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Prova_Ingresso::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $provas_ingresso = Prova_Ingresso::findOrFail($id);
+        return $provas_ingresso->update($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $provas_ingresso = Prova_Ingresso::findOrFail($id);
+        return $provas_ingresso->delete();
+
+    }
+}
