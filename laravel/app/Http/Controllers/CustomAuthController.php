f76cb99d5a56f4f7b48e466be45d7495cd490491
Diogo Rafael Silva Castro <a41746@alunos.ipb.pt>
2022-04-08 20:22:44 +0100

    Password Rules

--- laravel/app/Http/Controllers/CustomAuthController.php
+++ laravel/app/Http/Controllers/CustomAuthController.php
@@ -40,7 +40,15 @@ class CustomAuthController  extends Controller
        $request->validate([
            'name' => 'required',
            'email' => 'required|email|unique:users',
-           'password' => 'required|min:6',
+           'password' => [
+                       'required',
+                       'string',
+                       'min:10',             // must be at least 10 characters in length
+                       'regex:/[a-z]/',      // must contain at least one lowercase letter
+                       'regex:/[A-Z]/',      // must contain at least one uppercase letter
+                       'regex:/[0-9]/',      // must contain at least one digit
+                       'regex:/[@$!%*#?&]/', // must contain a special character
+           ],
        ]);
 
        $data = $request->all();
