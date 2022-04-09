623cd1b4d0f1cde6ac4dc7b0b291e45ecb5c80cc
Diogo Rafael Silva Castro <a41746@alunos.ipb.pt>
2022-04-09 15:54:33 +0100

    Show Username in navbar

--- laravel/resources/views/dashboard.blade.php
+++ laravel/resources/views/dashboard.blade.php
@@ -1,32 +1,36 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>DashBoard</title>
+    <title>Back-Office</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<nav class="navbar navbar-light navbar-expand-lg " style="background-color: #e3f2fd;">
-    <div class="container">
-        <a class="navbar-brand mr-auto" href="counter">Back-Office</a>
+<nav class="navbar navbar-expand-lg navbar-light navbar-expand-lg " style="background-color: #e3f2fd;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="counter">Back-Office</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 @guest
-                    <li class="nav-item">
+                    <li class="nav-item mr-auto">
                         <a class="nav-link" href="{{ route('login') }}">Login</a>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item mr-auto">
                         <a class="nav-link" href="{{ route('register-user') }}">Register</a>
                     </li>
                 @else
-                    <li class="nav-item">
+                    <li class="nav-item ml-auto">
+                        <a class="nav-link"> {{Auth::user()->name}}</a>
+                    </li>
+
+            </ul>
+                @endguest
+                    <li class="nav-item navbar-nav">
                         <a class="nav-link" href="{{ route('signout') }}">Logout</a>
                     </li>
-                @endguest
-            </ul>
         </div>
     </div>
 </nav>
