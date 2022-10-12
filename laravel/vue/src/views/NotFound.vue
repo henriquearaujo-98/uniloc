3c5faa997fb354eb793d8b0f6e4150ec3b51cd9a
Araújo, Henrique Luis <henriqueluis.araujo@student.fhws.de>
2022-10-12 11:28:46 +0000

    Not found page

---
+++ laravel/vue/src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+ <h1>404</h1>
+</template>
+
+<script>
+export default {
+    name: "NotFound"
+}
+</script>
+
+<style scoped>
+h1{
+    font-size: 2.3rem;
+    text-align: center;
+}
+</style>
